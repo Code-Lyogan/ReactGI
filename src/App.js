8d1ceb554feb7305fea08e6999ff8f1de7223030
Code-Lyogan <logantrout1@hotmail.com>
2022-12-19 23:09:53 -0500

    frst commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import BasicInfo from './components/BasicInfo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      person: {
+        name: '',
+        number: '',
+        birthDate: '',
+      },
+      people: [
+        {
+          name: "Logan Trout",
+          number: "704-632-5299",
+          birthDate: "02/28/1997",
+        },
+        {
+          name: "Grant Harrison",
+          number: "777-454-8442",
+          birthDate: "07/16/1999",
+        },
+        {
+          name: "Austin Horner",
+          number: "283-918-8227",
+          birthDate: "06/21/1997",
+        },
+      ]
+  }
 }
-
+  render() {
+    return this.state.people.map(function (person, index) {
+      return <BasicInfo key="{index}" person={person} />;
+    })
+  };
+}
 export default App;
